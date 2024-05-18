6f71d31c24bf6e4a7058307579d8c79309c25e83
chabiaknt <chabiaknt@gmail.com>
2024-05-18 08:32:33 +0530

    third update

--- src/app/name.js
+++ src/app/name.js
@@ -29,7 +29,7 @@ const NameForm = ({ onSubmit }) => {
       </label>
       <button
         type="submit"
-        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
       >
         Guess Info
       </button>
